ef5931fa1b277883987cbd67fa1b734245313979
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-10 14:04:43 -0800

    Completed basic layout of templates

--- Login_Registration_Pro/app_LR/views.py
+++ Login_Registration_Pro/app_LR/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hi Human!')
+    return render(request, 'index.html')
